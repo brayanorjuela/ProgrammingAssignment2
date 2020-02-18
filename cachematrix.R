635b5d9a7a115d87824d6007521d777371cb570b
Brayan Orjuela <brayansteven.3108@gmail.com>
2020-02-18 13:21:34 +0100

    Second Programming Assignment (Creation of the two functions)

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# My functions create a special type of matrix that through the appropriate use 
+# of lexical scoping can save valuable computational time by not calculating unnecessarily 
+# the inverse of a matrix that was already calculated. 
+
+
+# This function receives an argument to be converted into a matrix. This matrix is then going 
+# to be assigned to a variable inside a subsequent function and through the operator <<- 
+# is identifiable in the environment of the makeCacheMatrix function. After this, this function
+# creates a number of subsequent functions that allow the call of the given matrix and the input 
+# and call of the inverse of a matrix. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    inv <- NULL
+    setmatrix <- function(z) {
+      x <<- z
+      inv <<- NULL
+    }
+    getmatrix <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(setmatrix = setmatrix, getmatrix = getmatrix,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+# This function simply calls one of the functions defined in the special matrix 
+# to get the inverse of a matrix. Then, it will ouput this value, if it exists. Otherwise,
+# it will calculate the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+      message("getting cached data")
+      return(inv)
+    }
+    data <- x$getmatrix()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
+  
 }
